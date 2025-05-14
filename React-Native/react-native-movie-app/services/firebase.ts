acfefcd857151af33d5fdf76e2cdc9f68bb19699
Utkarsh Kohli <102008597+ImUKohli@users.noreply.github.com>
2025-05-15 00:57:29 +0530

    Update firebase.ts

--- React-Native/react-native-movie-app/services/firebase.ts
+++ React-Native/react-native-movie-app/services/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9wggbV11QWaNf3-3MAdZwIg9CwHTivCg",
-  authDomain: "book-store-mern-app-4e507.firebaseapp.com",
-  projectId: "book-store-mern-app-4e507",
-  storageBucket: "book-store-mern-app-4e507.firebasestorage.app",
-  messagingSenderId: "328149772752",
-  appId: "1:328149772752:web:6263a34277ad83756e71d5"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
